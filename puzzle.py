dd785035153487d460f0cc0512b83e29d1bbd14a
Castella Killian <killian.castella@he-arc.ch>
2018-03-27 12:50:49 +0200

    contours detection from an easy image

--- puzzle.py
+++ puzzle.py
@@ -2,13 +2,24 @@ import cv2
 import numpy as np
 	
 def readAndShowRGBImage():
-    imgRGB = cv2.imread("img/1.png")
+    imgRGB = cv2.imread("img/1.png",cv2.IMREAD_UNCHANGED)
+    channels = cv2.split(imgRGB)
+    # ADD COND TO CHECK IF IMG HAS 4 CHANNEL
+    #imgray = cv2.cvtColor(channels[3], cv2.COLOR_BGR2GRAY)
+    print(channels[3])
     if imgRGB is None:
         print("erreur ouverture fichier")
         return 0
-    cv2.imshow("Pièce", imgRGB)
+    height, width, channels = imgRGB.shape
+    print(height, width, channels)
+    cv2.imshow("Piece", imgRGB)
+    ret, thresh = cv2.threshold(channels[3], 127, 255, 0)
+    im2, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cv2.drawContours(imgRGB, contours, -1, (0,255,0), 3)
+    cv2.imshow("Piece2", imgRGB)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+    
 
 if __name__ == '__main__':
     readAndShowRGBImage()
---
+++ workWithContours.py
@@ -0,0 +1,21 @@
+import cv2
+import numpy as np
+	
+def readAndShowRGBImage():
+    imgRGB = cv2.imread("im2/contour.png",cv2.IMREAD_UNCHANGED)
+    if imgRGB is None:
+        print("erreur ouverture fichier")
+        return 0
+    imgray = cv2.cvtColor(imgRGB,cv2.COLOR_BGR2GRAY)
+    ret, thresh = cv2.threshold(imgray, 127, 255, 0)
+    im2, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    cv2.drawContours(imgRGB, contours, -1, (0,0,255), 3)
+    cv2.imshow("Piece2", imgRGB)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    
+
+if __name__ == '__main__':
+    readAndShowRGBImage()
+
+
