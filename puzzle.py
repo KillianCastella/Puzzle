ed608e6047f9cc6ece6dcff137fa69a866352617
Castella Killian <killian.castella@he-arc.ch>
2018-04-10 14:51:03 +0200

    detection contour

--- puzzle.py
+++ puzzle.py
@@ -2,20 +2,19 @@ import cv2
 import numpy as np
 	
 def readAndShowRGBImage():
-    imgRGB = cv2.imread("img/1.png",cv2.IMREAD_UNCHANGED)
+	#detection des contours
+    imgRGB = cv2.imread("img/9.png",cv2.IMREAD_UNCHANGED)
     channels = cv2.split(imgRGB)
-    # ADD COND TO CHECK IF IMG HAS 4 CHANNEL
-    #imgray = cv2.cvtColor(channels[3], cv2.COLOR_BGR2GRAY)
-    print(channels[3])
+    imgray = cv2.cvtColor(imgRGB, cv2.COLOR_BGR2GRAY)
     if imgRGB is None:
         print("erreur ouverture fichier")
         return 0
-    height, width, channels = imgRGB.shape
-    print(height, width, channels)
-    cv2.imshow("Piece", imgRGB)
     ret, thresh = cv2.threshold(channels[3], 127, 255, 0)
-    im2, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    cv2.drawContours(imgRGB, contours, -1, (0,255,0), 3)
+    im2, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    cv2.drawContours(imgRGB, contours, 0, (0,255,0), 3)
+    x,y,w,h=cv2.boundingRect(contours[0])
+    cv2.rectangle(imgRGB,(x,y),(x+w,y+h),(0,0,255),3)
+
     cv2.imshow("Piece2", imgRGB)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
--- workWithContours.py
+++ workWithContours.py
@@ -2,13 +2,14 @@ import cv2
 import numpy as np
 	
 def readAndShowRGBImage():
-    imgRGB = cv2.imread("im2/contour.png",cv2.IMREAD_UNCHANGED)
+    imgRGB = cv2.imread("img/1.png",cv2.IMREAD_UNCHANGED)
     if imgRGB is None:
         print("erreur ouverture fichier")
         return 0
     imgray = cv2.cvtColor(imgRGB,cv2.COLOR_BGR2GRAY)
     ret, thresh = cv2.threshold(imgray, 127, 255, 0)
     im2, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    print(contours)
     cv2.drawContours(imgRGB, contours, -1, (0,0,255), 3)
     cv2.imshow("Piece2", imgRGB)
     cv2.waitKey(0)
