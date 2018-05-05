e3ff3aa0672efd96f6bc93bc409b1e8dace74d24
Castella Killian <killian.castella@he-arc.ch>
2018-05-05 19:08:40 +0200

    separate sides

--- findcorner.py
+++ findcorner.py
@@ -1,11 +1,20 @@
 import cv2
 import numpy as np
 import os
+import math
+
+sideColor=[(39,253,61),(39,54,253),(240,8,225),(240,155,8),(255,255,255)]
 
 def distpp(p1,p2):
     return (p1[0]-p2[0])*(p1[0]-p2[0])+(p1[1]-p2[1])*(p1[1]-p2[1])
 
-    
+def isAngle(x,y,angles):
+    for index ,a in enumerate(angles):
+        if(math.sqrt(pow(x-a[0],2)+pow(y-a[1],2)) <5):
+            angles.pop(index)
+            return True
+    return False
+
 def findangle(datas, w, h):
     angles = []
     c1 = (0, 0)
@@ -78,11 +87,27 @@ def workimg(path):
             break
         except:   
             factor = factor + 0.02
-    cornersint = np.int0(corners)
+    cornersint = np.int0(centroids)
     height, width = img.shape[:2]
     angles=findangle(cornersint, width, height)
     posx, posy = zip(*angles)
-    img[posy[:], posx[:]] = [255,0,0]
+    
+    #side separation
+    sides=[]
+    currentside=0
+    sides.append([])
+    #make a list with angles
+    for x in range(0,contours[0].size // 2):
+        if(isAngle(contours[0][x][0][0],contours[0][x][0][1],angles)):
+            sides[currentside].append(contours[0][x][0])
+            currentside = currentside +1
+            sides.append([])
+        else:
+            sides[currentside].append(contours[0][x][0])
+
+    for l in range(0,len(sides)):
+        for s in sides[l]:
+            img[s[1],s[0]]=sideColor[l]
     cv2.imwrite("im2/"+path,img)
 
 def main():
